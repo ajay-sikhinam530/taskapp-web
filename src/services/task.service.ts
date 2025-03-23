8b77f43cf68c16e892ea79fe2ca8d29be43ab429
ajay_sikhinam <ajaysikhinam530@gmail.com>
2025-03-23 19:16:34 +0530

    added deploy workflow

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -7,7 +7,7 @@ import { Task } from '../models/task.model';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:8080/api/tasks'; // Update this if needed
+  private apiUrl = 'https://taskapp-tasks-api.azurewebsites.net/api/tasks'; // Update this if needed
 
   constructor(private http: HttpClient) {}
 
